38ead2cc89de20f92040e4be9f7c66c75c2e4066
Dmitriifd <idimka9@gmail.com>
2022-08-16 20:06:28 +0500

    Счетчик

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+    
+	const decrement = () => {
+		if (count >= 1) 
+			setCount(count - 1);
+	};
+
+	return (
+		<div className='App'>
+			<div>
+				<h2>Счетчик:</h2>
+				<h1>{count}</h1>
+				<button onClick={decrement} className='minus'>
+					- Минус
+				</button>
+				<button onClick={increment} className='plus'>
+					Плюс +
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
